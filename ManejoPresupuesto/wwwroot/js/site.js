3200d5eecdfdbb5c9a372072fe08f2c858d0228b
AndresEspitiaU <75282613+AndresFelipe23@users.noreply.github.com>
2023-12-23 12:39:53 -0500

    DB

--- ManejoPresupuesto/wwwroot/js/site.js
+++ ManejoPresupuesto/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+(function($) {
+
+	"use strict";
+
+	$('nav .dropdown').hover(function(){
+		var $this = $(this);
+		$this.addClass('show');
+		$this.find('> a').attr('aria-expanded', true);
+		$this.find('.dropdown-menu').addClass('show');
+	}, function(){
+		var $this = $(this);
+			$this.removeClass('show');
+			$this.find('> a').attr('aria-expanded', false);
+			$this.find('.dropdown-menu').removeClass('show');
+	});
+
+})(jQuery);
